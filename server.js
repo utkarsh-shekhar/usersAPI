b8034ef3762986fadaa7158dba001be0a804cbc7
utkarsh <utkarsh_me@yahoo.com>
2017-03-25 18:46:24 +0530

    Inital commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongo = require('mongodb')
+const mongoClient = mongo.MongoClient
+const url = 'mongodb://localhost/usersDB'
+const app = express()
+
+mongoClient.connect(url, function(err, db) {
+  if(err) return console.log(err)
+  let usersCollection = db.collection('users')
+
+  app.use(bodyParser.urlencoded({extended: true}))
+
+  app.listen(8081, function() {
+    console.log('Listening on 8081');
+  })
+
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+  })
+
+  app.post('/users/create', (req, res) => {
+
+    let insert = usersCollection.insert(req.body)
+    console.log(req.body)
+    if(insert)
+      res.send('insert successful', insert)
+    else
+      res.send('insert unsuccessful')
+  })
+
+  app.get('/users', (req, res) => {
+    usersCollection.find().toArray((err, results) => {
+      res.send(results)
+    })
+  })
+
+  app.get('/users/:id', (req, res) => {
+    console.log(req.params.id);
+    usersCollection.find({'_id': new mongo.ObjectId(req.params.id)}).toArray((err, results) => {
+      res.send(results)
+    })
+  })
+
+  app.get('/users/remove/:id', (req, res) => {
+    console.log(req.params.id);
+    usersCollection.remove({'_id': new mongo.ObjectId(req.params.id)}, (err, results) => {
+      if(err) return console.log(err)
+
+      res.send(results)
+    })
+  })
+})
